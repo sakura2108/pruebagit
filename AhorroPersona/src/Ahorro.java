26ff24c3f56bb46d80e0d625de7d513682fc6ee9
sakura2108 <mariadlab52@gmail.com>
2022-11-08 22:57:54 -0300

    estoy segura de este commit 1

---
+++ AhorroPersona/src/Ahorro.java
@@ -0,0 +1,30 @@
+/* Realice un diagrama de flujo y pseudocódigo que representen el algoritmo para
+determinar cuánto dinero ahorra una persona en un año si considera que cada semana
+ahorra 15% de su sueldo (considere cuatro semanas por mes y que no cambia el
+sueldo). */
+
+import java.util.Scanner;
+public class Ahorro {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+float sueldo;
+
+
+System.out.println("Ingrese el  monto del sueldo :");
+sueldo=sc.nextFloat();
+
+double semana= sueldo*0.15;   // semana= ahorro por semana
+double mes= semana*4;        //mes= ahorro por mes    
+double año= semana*12;       // año0 ahorro por año
+
+
+System.out.println("El ahorro anual es de: "+)
+
+
+		
+	}
+
+}

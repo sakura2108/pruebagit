2e620f4c5a282a5424b094e6a59a5247177ff9f0
sakura2108 <mariadlab52@gmail.com>
2022-12-05 17:55:50 -0300

    guia-poo

---
+++ GuiaPooEjericicio7/src/Entidades/main.java
@@ -0,0 +1,15 @@
+package Entidades;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Persona persona1=new Persona();
+		
+		persona1.crearpersona();
+	    persona1.calcularIMC();
+	    persona1.esMayordeEdad();
+	    persona1.mostrar();
+	}
+
+}
